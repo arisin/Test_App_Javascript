73d1f9b6a097dfeb1ba0869058fda8ff59d526b4
arisin <monokesi4120@gmail.com>
2012-05-21 11:22:22 +0900

    tweet6

--- js/tubyaki_Js.js
+++ js/tubyaki_Js.js
@@ -77,19 +77,20 @@ var twitter_count = 10; //表示数
 var twitter_elem_id = 'twitter_data';
 
 window.onload = function() {
-var url = 'http://twitter.com/statuses/user_timeline/'
-+ twitter_id
-+ '.json?callback=twitterCallback&count='
-+ twitter_count;
-var script = document.createElement('script');
-script.setAttribute('src', url);
-document.body.appendChild(script);
+	var url = 'http://twitter.com/statuses/user_timeline/'
+				+ twitter_id
+				+ '.json?callback=twitterCallback&count='
+				+ twitter_count;
+	var script = document.createElement('script');
+	script.setAttribute('src', url);
+	document.body.appendChild(script);
 }
+
 function twitterCallback(obj){
-var html = "";
-for(var i=0 ; i<twitter_count && i<obj.length ; i++){
-html += "<li><a href='http://twitter.com/" + twitter_id + "/status/"
-+ obj[i].id + "'>" + obj[i].text + "</a></li>";
-}
-document.getElementById('t_line').innerHTML = html;
+	var html = "";
+	for(var i=0 ; i<twitter_count && i<obj.length ; i++){
+		html += "<li><a href='http://twitter.com/" + twitter_id + "/status/"
+		+ obj[i].id + "'>" + obj[i].text + "</a></li>";
+	}
+	document.getElementById('t_line').innerHTML = html;
 }
