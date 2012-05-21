1a67047d0d192b611a815e4be9f7e420c1083cc2
arisin <monokesi4120@gmail.com>
2012-05-21 11:18:21 +0900

    tweet5

--- js/tubyaki_Js.js
+++ js/tubyaki_Js.js
@@ -71,3 +71,25 @@ twttr.anywhere(function(t){
       	defaultContent: "ツイートする"
 	});
  });
+ 
+var twitter_id = 'arisinn'; //表示したいtwitter ID
+var twitter_count = 10; //表示数
+var twitter_elem_id = 'twitter_data';
+
+window.onload = function() {
+var url = 'http://twitter.com/statuses/user_timeline/'
++ twitter_id
++ '.json?callback=twitterCallback&count='
++ twitter_count;
+var script = document.createElement('script');
+script.setAttribute('src', url);
+document.body.appendChild(script);
+}
+function twitterCallback(obj){
+var html = "";
+for(var i=0 ; i<twitter_count && i<obj.length ; i++){
+html += "<li><a href='http://twitter.com/" + twitter_id + "/status/"
++ obj[i].id + "'>" + obj[i].text + "</a></li>";
+}
+document.getElementById('t_line').innerHTML = html;
+}
